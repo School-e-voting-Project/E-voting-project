820021da5588615a0b4405cd3d242c6634455330
Ebun <oyebolaebun07@gmail.com>
2023-12-09 21:57:52 +0100

    solved error

--- src/constants/contestants.js
+++ src/constants/contestants.js
@@ -1,4 +1,4 @@
-import { img1, img2, img3, img4, img5, img6, img7 } from "@/assets/images";
+import { img1, img2, img3, img4, img5, img6, img7 } from "@/assets/Images";
 
 const contestants = [
   {
